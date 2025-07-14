7fbb3542aea0d50893a3ee507b3c8878ff9963cc
sudoRaul <rrodfer05@gmail.com>
2025-07-14 16:38:39 +0200

    Product list done

---
+++ src/components/ProductCart.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+    title: String,
+    price: Number,
+    description: String,
+    category: String,
+    imgSource: String,
+    rating: Number
+})
+</script>
+<template>
+    <div class="w-64 h-96 flex flex-col justify-between rounded-xl shadow-lg p-4 bg-white">
+        <!-- Imagen con tamaño fijo -->
+        <div class="h-40 flex items-center justify-center">
+            <img :src="imgSource" alt="Product image" class="h-full object-contain" />
+        </div>
+
+        <!-- Info del producto -->
+        <div class="mt-4">
+            <p class="text-center font-semibold line-clamp-2 h-12">{{ title }}</p>
+            <p class="text-center font-bold text-green-600 mt-2">{{ price }} €</p>
+        </div>
+    </div>
+
+    <!--
+        <h2 class="text-center">{{ title }}</h2>
+        <p>{{ price }}</p>
+        <p>{{ category }} </p>
+        <p>{{ description }}</p>
+        
+        <div>
+            <span v-for="star in Math.ceil(rating, 1)" :key="star">⭐</span>
+        </div>
+        -->
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,34 @@
 <script setup>
+import Card from '@/components/Card.vue'
+import ProductCart from '@/components/ProductCart.vue';
+import { onMounted, ref } from 'vue';
+
+const products = ref([])
+
+async function getProducts() {
+    try {
+        const response = await fetch("https://fakestoreapi.com/products")
+
+        if (!response.ok) {
+            console.error("Error al obtener los productos")
+        }
+
+        products.value = await response.json()
+    } catch (err) {
+        
+    }
+}
+
+onMounted(getProducts)
 </script>
 
 <template>
-  
+    <h2 class="text-center text-3xl mb-10">Products Available</h2>
+    <div class="flex flex-wrap gap-10 justify-center items-stretch">
+        <div v-for="product in products" :key="product.id" class="flex flex-col justify-between h-full">
+            <ProductCart :title="product.title" :price="product.price"
+            :description="product.description" :category="product.category" :imgSource="product.image" :rating="product.rating.rate" />
+        </div>
+    </div>
+    
 </template>
